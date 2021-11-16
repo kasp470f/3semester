c636df55d108959bb2048b51daa9b67575d4de8c
Natasha C. B. R <natashacbr@outlook.com>
2021-11-16 09:31:12 +0100

    User Story 8 Information page

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace WebApplication.Controllers
             return View();
         }
 
+        public IActionResult Info()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
