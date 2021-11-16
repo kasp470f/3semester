fb5699c0b08144a8228156efeec7bf31eab15efe
Kasper <kw2011@live.dk>
2021-11-16 21:55:39 +0100

    Removed Packages that was not used

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Diagnostics;
 using WebApplication.Models;
 
