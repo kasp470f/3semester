312c2744153f38f8c834708d5e4d1aab726d1c65
Kasper <kw2011@live.dk>
2021-11-18 20:21:34 +0100

    Custom Identity

---
+++ WebApplication/Areas/Identity/Data/AccountIdentity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the AccountIdentity class
+    public class AccountIdentity : IdentityUser
+    {
+        [PersonalData]
+        public string FullName { get; set; }
+
+        [PersonalData]
+        public string Address { get; set; }
+    }
+}
---
+++ WebApplication/Data/StoholmDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication.Models
+{
+    public class StoholmDbContext : DbContext
+    {
+        public StoholmDbContext(DbContextOptions<StoholmDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Participant> Participants { get; set; }
+    }
+}
