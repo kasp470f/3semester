9c5c39c9a743be5e545dd15391a9fd6885b57d9f
Kasper <kw2011@live.dk>
2021-11-18 20:21:28 +0100

    Removed for custom identity

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApplication.Areas.Identity.Data;
 using WebApplication.Data;
 using WebApplication.Models;
 using WebApplication.Models.Repositories;
@@ -23,10 +24,7 @@ namespace WebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationIdentityDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationIdentityDbContext>();
             services.AddDbContext<StoholmDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddAuthentication().AddFacebook(facebookOptions => 
             {
