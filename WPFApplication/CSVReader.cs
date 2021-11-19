4f8c45a1a92d6513b966800b5b1eeab139acf453
Rulz123 <rullerbjerg@hotmail.com>
2021-11-19 13:05:15 +0100

    Fixed corruption

---
+++ WPFApplication/CSVReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFApplication
+{
+    public class CSVReader
+    {
+        //public static ReadCSV()
+        //{
+
+        //}
+    }
+}
--- WPFApplication/MainWindow.xaml.cs
+++ WPFApplication/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,17 @@ namespace WPFApplication
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Folder_Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void Upload_Button_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
