880950af0ba0b0a9855e0e4ad5f5c888a4e058cf
Kasper <kw2011@live.dk>
2021-11-25 10:29:09 +0100

    Comments

--- WPFApplication/MainWindow.xaml.cs
+++ WPFApplication/MainWindow.xaml.cs
@@ -12,17 +12,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFApplication.ViewModel;
+using WPFApplication.Views;
 
 namespace WPFApplication
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// <para>Created by Rezan</para>
     /// </summary>
     public partial class MainWindow : Window
     {
+        UserControl uc;
+        // Created by Rezan
         public MainWindow()
         {
             InitializeComponent();
+            uc = new UserControl();
+            
+        }
+
+        // Created by Rezan
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+            
+            uc = new Import();
+            MainView.Children.Clear();
+            MainView.Children.Add(uc);
+            uc.Visibility = Visibility.Visible;
+
+        }
+
+        // Created by Rezan
+        private void btnUpload_Click(object sender, RoutedEventArgs e)
+        {
+            uc = new Upload();
+            MainView.Children.Clear();
+            MainView.Children.Add(uc);
+            uc.Visibility = Visibility.Visible;
+
         }
     }
 }
---
+++ WPFApplication/Model/Participate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFApplication.Model
+{
+    public class Participate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
